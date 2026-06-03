52f9b5889a57726cceed2012fcd1835c9bc9dd98
Raghuram G <raghuram.g2024cse@sece.ac.in>
2026-06-04 00:12:27 +0530

    Delete modern-fs-internship-day-1/modern-fs-internship-day-1 directory

--- modern-fs-internship-day-1/modern-fs-internship-day-1/dist/intersection.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const emp1 = {
-    name: "Raghu",
-    age: 30,
-    salary: 50000,
-    // employeeId: 101
-};
-console.log("Employee 1 details:", emp1);
